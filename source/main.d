c6cf6bede64a09aa794398388b31ac897121bcfc
tfcat <danielbri3r@gmail.com>
2018-11-05 23:54:51 +0000

    Updated main demo as per updates

--- source/main.d
+++ source/main.d
@@ -4,34 +4,45 @@ import screen;
 import assets;
 import game;
 import keyboard;
+import maps;
 
 import std.format;
 
 void main()
 {  
-  screen.init("oofy", 640, 480, 2);  
+  screen.init("oofy", 640, 480, 2, true);  
+  maps.load_map("maps/untitled.json","images/shrinetiles.png");
   game.add_object(new Girl(50, 50));
   game.enter_event_loop();
 }
 
-
-class Girl : GameObject {
+class Girl : GameObject 
+{
   double sprite_index = 0;
   Sprite main_sprite;
 
-  this(int ix, int iy){
+  enum Animation : int[] {
+    walk_up = [38,39,40,41,42,43,45,46]
+  };
+
+  Animation current_animation = Animation.walk_up;
+  double animation_speed = 0.2;
+
+  this(int ix, int iy)
+  {
     x = ix;
     y = iy;
-    main_sprite = new Sprite("images/tg.png", 32, 32);
+    main_sprite = new Sprite("images/dora.png", 32, 32);
   }
 
-  override void step() {
-    sprite_index = sprite_index+0.1;
-    if(sprite_index >= 4) sprite_index=0;
+  override void step()
+  {
+    sprite_index = sprite_index + animation_speed;
+    if(sprite_index >= current_animation.length) sprite_index=0;
     
-    screen.draw_sprite(main_sprite, cast(int)sprite_index, x, y);
+    screen.draw_sprite(main_sprite, current_animation[cast(int)sprite_index], x, y);
 
-    if(keyboard.is_down("A")){
+    if(keyboard.is_down("Up")){
       assets.draw_debug_text("A has been pressed!");
     }
   }
