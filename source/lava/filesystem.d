821b8f5822b5ec9905485aad9c59672e40dfda1e
Daniel <danbrierly@gmail.com>
2019-03-17 17:24:31 +0000

    Compile-time asset packing could be a thing soon!

--- source/lava/filesystem.d
+++ source/lava/filesystem.d
@@ -3,6 +3,22 @@
         - Reference assets by a string identifier.
         - Decompress / unencrypt data depending on how it's stored.
         - Return assets upon request.
+
+    TODO: compile-time packing of assets, accessible by a virtual system.
 */
 
 module lava.filesystem;
+
+private string __buildResources() {
+    if(!__ctfe) return "CANNOT CALL __buildResources AT RUNTIME";
+     
+    // Called at compile-time
+    pragma(msg, "Running");
+
+    //compile assets here
+    
+    return "CTFE_Running at compile-time.";
+}
+    
+pragma(msg, __buildResources());
+
